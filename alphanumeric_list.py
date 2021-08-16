1375af33742feb0791acaec58e2792c77c8ecf39
atmygml <43545518+atmygml@users.noreply.github.com>
2021-08-16 17:27:33 +0800

    Update alphanumeric_list.py

--- alphanumeric_list.py
+++ alphanumeric_list.py
@@ -1,13 +1,9 @@
 import string
 
-
 print(string.ascii_lowercase + string.ascii_uppercase + string.digits)
 print("\n")
 print(list(string.ascii_lowercase + string.ascii_uppercase + string.digits))
 
-
 alphanumeric_str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 alphanumeric_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-
